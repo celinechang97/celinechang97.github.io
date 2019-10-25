97f48dadda72a3bcc9f5803227c41c19bce69786
Celine Chang <celinec@andrew.cmu.edu>
2019-10-24 20:06:48 -0400

    more for hw 6b (added all the other detail html)

--- homework_6B/bunbun.js
+++ homework_6B/bunbun.js
@@ -36,8 +36,44 @@ function showSlides(n) {
 
 
 
+/* GETTING THE TYPE OF BUN*/
+var typeOfBun = "";
 
+function getTypeOfBun(ele){
+  if (ele === 0){
+    typeOfBun = "Original";
+    localStorage.setItem("storedTypeOfBun", typeOfBun);
+  }
 
+  else if (ele === 1){
+    typeOfBun = "Blackberry";
+    localStorage.setItem("storedTypeOfBun", typeOfBun);
+  }
+
+  else if (ele === 2){
+    typeOfBun = "Walnut";
+    localStorage.setItem("storedTypeOfBun", typeOfBun);
+  }
+
+  else if (ele === 3){
+    typeOfBun = "Original - GF";
+    localStorage.setItem("storedTypeOfBun", typeOfBun);
+  }
+
+  else if (ele === 4){
+    typeOfBun = "Pumpkin Spice";
+    localStorage.setItem("storedTypeOfBun", typeOfBun);
+  } 
+
+  else if (ele === 5){
+    typeOfBun = "Caramel Pecan";
+    localStorage.setItem("storedTypeOfBun", typeOfBun);
+  }  
+}
+
+function displayTypeOfBun(){
+  typeOfBun = localStorage.getItem("storedTypeOfBun");
+}
 
 /* FOR QUANTITY SELECTION INDICATION ON DETAILS.HTML*/
 /* AND */
@@ -153,13 +189,6 @@ var count_int = 0;
 
 /*
 function cartCount(){
-  count_int += 1;
-  document.getElementById("cart-count").innerHTML = count_int;
-  localStorage.setItem("number_of_items",count_int);
-}
-*/
-
-function cartCount(){
   if (typeof(Storage) === "undefined"){
     document.getElementById("cart-count").innerHTML = "0";
   }
@@ -172,11 +201,21 @@ function cartCount(){
   }
   document.getElementById("cart-count").innerHTML = localStorage.clickcount;
 }
+*/
+
+function cartCount(){
+  count_int +=1;
+  count_int_serialized = JSON.stringify(count_int);
+  localStorage.setItem("storedCount", count_int_serialized);
+  displayCartCount();
+}
+
 
 function displayCartCount(){
-  document.getElementById("cart-count").innerHTML = localStorage.clickcount;
-
+  count_int_deserialized = JSON.parse(localStorage.getItem("storedCount"));
+  document.getElementById("cart-count").innerHTML = count_int_deserialized;
 }
+
 
 
 
@@ -196,6 +235,7 @@ var errorMsg = document.getElementById("errorMessage");
 var cart = [];
 var cart_serialized = [];
 var cart_deserialized = [];
+var priceOfBun = 0;
 
 
 function addItem(item){
@@ -207,6 +247,8 @@ function addItem(item){
 function addingtoCart(){
   if (thisIsQuantity === 0 || thisIsGlaze === ""){
     errorMsg.setAttribute("id", "showErrorMessage");
+    var element = document.getElementById("add-button");
+    element.classList.add("changing");
   }
 
   else {
@@ -214,9 +256,20 @@ function addingtoCart(){
     cartCount();
     addButton.innerHTML = "Added!";
     
-    var item_name = "Original + " + thisIsGlaze;
-    var total_price = 3.5*thisIsQuantity;
-    var item = new Item(item_name, 3.50, thisIsQuantity, total_price);
+    var item_name = typeOfBun + " + " + thisIsGlaze;
+
+    if (typeOfBun === "Original"){
+      priceOfBun = 3;
+    }
+    else if (typeOfBun === "Blackberry" || typeOfBun === "Walnut" || typeOfBun === "Original - GF"){
+      priceOfBun = 3.5;
+    }
+    else if (typeOfBun === "Pumpkin Spice" || typeOfBun === "Caramel Pecan"){
+      priceOfBun = 4;
+    }
+
+    var total_price = priceOfBun*thisIsQuantity;
+    var item = new Item(item_name, priceOfBun, thisIsQuantity, total_price);
     addItem(item);
     cart_serialized = JSON.stringify(cart);
     localStorage.setItem("storedCart", cart_serialized);
@@ -224,6 +277,11 @@ function addingtoCart(){
 
 }
 
+/*
+function displayCart(){
+  cart = JSON.parse(localStorage.getItem("storedCart"));
+}
+*/
 
 
 
@@ -233,14 +291,36 @@ var parentDiv = document.getElementById("cart");
 var childDiv = "";
 var subtotal = 0;
 var tax = 0;
+var shipping = 3;
 var total = 0;
+var picture = "";
 
 function createCart(){
   cart_deserialized = JSON.parse(localStorage.getItem("storedCart"));
+  typeOfBun = localStorage.getItem("storedTypeOfBun");
 
   for (i=0; i<cart_deserialized.length; i++){
    var current_item = cart_deserialized[i];
    subtotal += current_item.totalPrice;
+
+   if (typeOfBun === "Original"){
+    picture = "original.jpg";
+   }
+   else if (typeOfBun === "Blackberry"){
+    picture = "blackberrybun.jpg";
+   }
+   else if (typeOfBun === "Walnut"){
+    picture = "walnutbun.jpg";
+   }
+   else if (typeOfBun === "Original - GF"){
+    picture = "originalgf.jpg";
+   }
+   else if (typeOfBun === "Pumpkin Spice"){
+    picture = "pumpkinbun.jpg";
+   }
+   else if (typeOfBun === "Caramel Pecan"){
+    picture = "pecanbun.jpg";
+   }
    
    //these are the keys
    /*
@@ -249,35 +329,72 @@ function createCart(){
    current_item.quantity.toString();
    current_item.totalPrice.toString();
    */
-
-   childDiv += "<div id=\"" + "id" + i + "\" class=\"cart-row cart-product\"><div id=\"cart-item-image\" class=\"cart-product-column\"> <img class=\"imagee\" src=\"original.jpg\" alt=\"blackberry bun\"> </div><div id=\"cart-item-desc\" class=\"cart-product-column\">" + current_item.name + "</div><div id=\"cart-item-price\" class=\"cart-product-column\">" + "$" + current_item.price.toFixed(2).toString() + "</div><div id=\"cart-item-quantity\" class=\"cart-product-column\">" + current_item.quantity.toString() + "</div><div id=\"cart-item-total-price\" class=\"cart-product-column\">" + "$" + current_item.totalPrice.toFixed(2).toString() + "</div><div onclick=\"removeItem(" + i + ")\" id=\"cart-item-remove\" class=\"cart-product-column\">remove</div></div>";
+ 
+   childDiv += "<div id=\"" + "id" + i + "\" class=\"cart-row cart-product\"><div id=\"cart-item-image\" class=\"cart-product-column\"> <img class=\"imagee\" src=\"" + picture + "\" " + "alt=\"cinbun\"> </div><div id=\"cart-item-desc\" class=\"cart-product-column\">" + current_item.name + "</div><div id=\"cart-item-price\" class=\"cart-product-column\">" + "$" + current_item.price.toFixed(2).toString() + "</div><div id=\"cart-item-quantity\" class=\"cart-product-column\">" + current_item.quantity.toString() + "</div><div id=\"cart-item-total-price\" class=\"cart-product-column\">" + "$" + current_item.totalPrice.toFixed(2).toString() + "</div><div onclick=\"removeItem(" + i + ")\" id=\"cart-item-remove\" class=\"cart-product-column\">remove</div></div>";
   }
 
   parentDiv.innerHTML += childDiv;
   
   tax = subtotal*0.1;
-  total = tax + subtotal;
+  total = subtotal + shipping + tax;
   document.getElementById("subtotal").innerHTML = "$" + subtotal.toFixed(2).toString();
+  document.getElementById("shipping").innerHTML = "$" + shipping.toFixed(2).toString();
   document.getElementById("tax").innerHTML = "$" + tax.toFixed(2).toString();
   document.getElementById("total").innerHTML = "$" + total.toFixed(2).toString();
 }
 
 
+/*
+function confirmRemove(ele){
+  console.log(ele);
+  var txt;
+  if (confirm("Are you sure you want to remove this item?")) {
+    removeItem(ele);
+  } 
+}
+*/
 
 
 function removeItem(ele){
   for (i=0; i<cart_deserialized.length;i++){
     if (i === ele){
+      //REMOVING IT FROM SCREEN
       var stringi = i.toString();
       var theid = "id"+stringi;
       var theElement = document.getElementById(theid);
       theElement.parentNode.removeChild(theElement);
+
+
+      //CHANGING ORDER SUMMARY (COST)
+      var removingCost = cart_deserialized[i].totalPrice;
+      subtotal -= removingCost;
+      document.getElementById("subtotal").innerHTML = "$" + subtotal.toFixed(2).toString();
+      tax = subtotal*0.1;
+      document.getElementById("tax").innerHTML = "$" + tax.toFixed(2).toString();
+      total = subtotal + shipping + tax;
+      document.getElementById("total").innerHTML = "$" + total.toFixed(2).toString();
+
+
+      //UPDATING LOCAL STORAGE
+      cart_deserialized.splice(i, 1);
+      count_int_deserialized-=1
+      count_int = count_int_deserialized
+      count_int_serialized = JSON.stringify(count_int);
+      localStorage.setItem("storedCount", count_int_serialized)
+      displayCartCount();
+      cart = cart_deserialized;
+      cart_serialized = JSON.stringify(cart);
+      localStorage.setItem("storedCart", cart_serialized);
     }
   }
 }
 
 
 
+
+
+
+// NEXT TO DO = ONLOAD FOR BUN, DESERIALIZE THE UPDATED CART (because i pushed the edited cart to local storage as cart)
 
 
 
