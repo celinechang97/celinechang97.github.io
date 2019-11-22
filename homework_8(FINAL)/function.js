bfc990e975286e96b7272d7ce15bbc0058a21229
Celine Chang <celinec@andrew.cmu.edu>
2019-11-21 21:11:47 -0500

    yayyy responsive and smooth scroll

---
+++ homework_8(FINAL)/function.js
@@ -0,0 +1,40 @@
+/* FOR SMOOTH SCROLL */
+$('.centerGroup a').on('click', function(e) {
+	if(this.hash !== ''){
+		e.preventDefault();
+
+		const hash = this.hash;
+
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 
+		800
+		);
+	}
+});
+
+
+
+
+/* FOR BACK TO TOP BUTTON*/
+
+//Get the button
+var mybutton = document.getElementById("top_btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  console.log("scroll");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
