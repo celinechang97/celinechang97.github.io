84cc7cb930484b60ff9e7814d2c0b1d3f9e1c18d
Celine Chang <celinec@andrew.cmu.edu>
2019-11-24 16:58:53 -0500

    scrolling animation

--- homework_8(FINAL)/function.js
+++ homework_8(FINAL)/function.js
@@ -1,3 +1,30 @@
+/*TITLE ANIMATION*/
+$(function(){
+	setTimeout(function(){
+		$('.title').removeClass('hidden');
+	}, 900);
+});
+
+$(function(){
+	setTimeout(function(){
+		$('.threeSectionsGroup').removeClass('hidden');
+	}, 2000);
+});
+
+$(function(){
+	setTimeout(function(){
+		$('#flower1').removeClass('hidden');
+	}, 100);
+});
+
+$(function(){
+	setTimeout(function(){
+		$('#coverPhoto').removeClass('hidden');
+	}, 200);
+});
+
+
+
 /* FOR SMOOTH SCROLL */
 $('.centerGroup a').on('click', function(e) {
 	if(this.hash !== ''){
@@ -8,7 +35,7 @@ $('.centerGroup a').on('click', function(e) {
 		$('html, body').animate({
 			scrollTop: $(hash).offset().top
 		}, 
-		800
+		1500
 		);
 	}
 });
@@ -38,3 +65,90 @@ function scrollFunction() {
 //   document.body.scrollTop = 0;
 //   document.documentElement.scrollTop = 0;
 // }
+
+
+
+
+
+/* FOR ANIMATION */
+var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl2 = new TimelineMax({onUpdate:updatePercentage});
+var tl3 = new TimelineMax({onUpdate:updatePercentage});
+var tl4 = new TimelineMax({onUpdate:updatePercentage});
+var tl5 = new TimelineMax({onUpdate:updatePercentage});
+var tl6 = new TimelineMax({onUpdate:updatePercentage});
+const controller = new ScrollMagic.Controller();
+
+tl.from('#typesofyogaHeading', 30, {x:-100, opacity: 0});
+tl.from('#typesofyogaBG', 10, {x:50, opacity: 0});
+
+tl2.from('#benefitsofyogaHeading', 9000, {x:-300, opacity: 0});
+tl2.from('#benefitsofyogaBG', 2000, {x:50, opacity: 0});
+tl2.from('#leftDescription', 4000, {y:-100, opacity: 0});
+tl2.from('#rightDescription', 4000, {x:100, opacity: 0});
+
+tl3.from('#posturesofyogaHeading', 180, {x:-100, opacity: 0});
+tl3.from('#posturesofyogaBG', 180, {x:50, opacity: 0});
+
+tl4.from('.scrolling-wrapper', 180, {y:500, opacity: 0});
+
+tl5.from('#ashtangaDescription', 180, {y:500, opacity: 0});
+
+tl6.from('#ashtangaTitle', 30, {x:500, opacity: 0});
+
+
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: "#secondSection",
+            triggerHook: "onEnter",
+            duration: "100%"
+})
+  .setTween(tl)
+    .addTo(controller);
+
+
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "#thirdSection",
+            triggerHook: "onEnter",
+            duration: "100%"
+})
+  .setTween(tl2)
+    .addTo(controller);
+
+
+const scene3 = new ScrollMagic.Scene({
+  triggerElement: "#fourthSection",
+            triggerHook: "onEnter",
+            duration: "100%"
+})
+  .setTween(tl3)
+    .addTo(controller);
+
+const scene4 = new ScrollMagic.Scene({
+  triggerElement: "#posturesofyogaHeading",
+            triggerHook: "onEnter",
+            duration: "100%"
+})
+  .setTween(tl4)
+    .addTo(controller);
+
+const scene5 = new ScrollMagic.Scene({
+  triggerElement: "#secondSection",
+            triggerHook: "onEnter",
+            duration: "100%"
+})
+  .setTween(tl5)
+    .addTo(controller);
+
+const scene6 = new ScrollMagic.Scene({
+  triggerElement: "#secondSection",
+            triggerHook: "onEnter",
+            duration: "100%"
+})
+  .setTween(tl6)
+    .addTo(controller);
+
+function updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+  tl.progress();
+}
